c171e7175e148e94448f110e531b81a45d096559
BitForgeCraftedCode <11381086+BitForgeCraftedCode@users.noreply.github.com>
2025-01-27 20:00:00 -0400

    add folder model classes

---
+++ ImagePerfect/Models/Folder.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ImagePerfect.Models
+{
+    [Table("folders")]
+    public class Folder
+    {
+        [Key]
+        [Column("FolderId")]
+        public int FolderId { get; set; }
+        [Column("FolderName")]
+        public string FolderName { get; set; } = string.Empty;
+        [Column("FolderPath ")]
+        public string FolderPath { get; set; } = string.Empty;
+        [Column("HasChildren")]
+        public bool HasChildren { get; set; }
+        [Column("CoverImagePath ")]
+        public string? CoverImagePath { get; set; }
+        [Column("FolderDescriptio")]
+        public string? FolderDescription { get; set; }
+        [Column("FolderTags")]
+        public string? FolderTags { get; set; }
+        [Column("FolderRating")]
+        public int FolderRating { get; set; }
+        [Column("HasFiles")]
+        public bool HasFiles { get; set; }
+        [Column("IsRoot")]
+        public bool IsRoot { get; set; }
+        [Column("FolderContentMetaDataScanned")]
+        public bool FolderContentMetaDataScanned { get; set; }
+    }
+}
---
+++ ImagePerfect/Models/FolderCsv.cs
@@ -0,0 +1,19 @@
+﻿
+
+namespace ImagePerfect.Models
+{
+    public class FolderCsv
+    {
+        public int FolderId { get; set; }
+        public string FolderName { get; set; } = string.Empty;
+        public string FolderPath { get; set; } = string.Empty;
+        public int HasChildren { get; set; }
+        public string? CoverImagePath { get; set; }
+        public string? FolderDescription { get; set; }
+        public string? FolderTags { get; set; }
+        public int FolderRating { get; set; }
+        public int HasFiles { get; set; }
+        public int IsRoot { get; set; }
+        public int FolderContentMetaDataScanned { get; set; }
+    }
+}
