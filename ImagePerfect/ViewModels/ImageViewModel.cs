9dbfb94e7ce43f115ee5603de04512727fb42d6c
BitForgeCraftedCode <11381086+BitForgeCraftedCode@users.noreply.github.com>
2025-02-02 20:00:00 -0400

    add image veiw model

---
+++ ImagePerfect/ViewModels/ImageViewModel.cs
@@ -0,0 +1,54 @@
+using Avalonia.Media.Imaging;
+using ReactiveUI;
+
+namespace ImagePerfect.ViewModels
+{
+	public class ImageViewModel : ViewModelBase
+	{
+		private int _imageId;
+		private Bitmap _imagePath;
+		private string? _imageTags;
+		private int _imageRating;
+		private string _imageFolderPath;
+		private bool _imageMetaDataScanned;
+		private int _folderId;
+
+		public int ImageId
+		{
+			get => _imageId;
+			set => this.RaiseAndSetIfChanged(ref _imageId, value);
+		}
+
+		public Bitmap ImagePath
+		{
+			get => _imagePath;
+			set => this.RaiseAndSetIfChanged(ref _imagePath, value);
+		}
+		public string? ImageTags
+		{
+			get => _imageTags;
+			set => this.RaiseAndSetIfChanged(ref _imageTags, value);
+		}
+		public int ImageRating
+		{
+			get => _imageRating;
+			set => this.RaiseAndSetIfChanged(ref _imageRating, value);
+		}
+		public string ImageFolderPath
+		{
+			get => _imageFolderPath;
+			set => this.RaiseAndSetIfChanged(ref _imageFolderPath, value);
+		}
+		public bool ImageMetaDataScanned
+		{
+			get => _imageMetaDataScanned;
+			set => this.RaiseAndSetIfChanged(ref _imageMetaDataScanned, value);
+		}
+		public int FolderId
+		{
+			get => _folderId;
+			set => this.RaiseAndSetIfChanged(ref _folderId, value);
+		}
+
+	}
+}
