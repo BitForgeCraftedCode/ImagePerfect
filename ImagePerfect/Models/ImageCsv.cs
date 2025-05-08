8a4d048ab25f08dd39f5de28c133ae0bf1df9a39
BitForgeCraftedCode <11381086+BitForgeCraftedCode@users.noreply.github.com>
2025-05-07 20:00:00 -0400

    remove commented line

--- ImagePerfect/Models/ImageCsv.cs
+++ ImagePerfect/Models/ImageCsv.cs
@@ -5,7 +5,6 @@
         public int ImageId { get; set; }
         public string ImagePath {  get; set; }
         public string FileName { get; set; }
-        //public string? ImageTags {  get; set; } = string.Empty;
         public int  ImageRating {  get; set; }
         public string ImageFolderPath { get; set; }
         public int ImageMetaDataScanned { get; set; }
